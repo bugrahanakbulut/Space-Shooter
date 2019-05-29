3a80181e9552cf8becdef4b2fb7d3e2294554d05
Bugrahan Akbulut <b21426529@cs.hacettepe.edu.tr>
2019-05-29 17:26:05 +0300

    Added Laser and Laser Behavior

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    private float speed = 10.0f;
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movementController(); 
+	}
+
+    private void movementController()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        
+        if (transform.position.y >= 6.0f)
+            Destroy(gameObject);
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public GameObject laserPrefab;
+
+    private float fireRate = 0.25f;
+    private float canFire = 0.0f;
+    
     private float speed = 7.5f;
 
 
@@ -15,6 +20,7 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         movementController();
+        laserController();
 	}
 
     private void movementController()
@@ -26,11 +32,13 @@ public class Player : MonoBehaviour {
         Vector3 horizontalDirection = Vector3.right * speed * horizontalInput * Time.deltaTime;
         Vector3 verticalDirection = Vector3.up * speed * verticalInput * Time.deltaTime;
 
-        if (transform.position.x < -6.5f)
-            transform.position = new Vector3(-6.5f, transform.position.y, transform.position.z);
-        else if (transform.position.x > 8.0f)
-            transform.position = new Vector3(8.0f, transform.position.y, transform.position.z);
+        // Player movement limitations on x-axis
+        if (transform.position.x < -6.0f)
+            transform.position = new Vector3(-6.0f, transform.position.y, transform.position.z);
+        else if (transform.position.x > 6.0f)
+            transform.position = new Vector3(6.0f, transform.position.y, transform.position.z);
 
+        // Player movement limitations on y-axis
         if (transform.position.y < -3.5f)
             transform.position = new Vector3(transform.position.x, -3.5f, transform.position.z);
         else if (transform.position.y > -1.0f)
@@ -38,5 +46,21 @@ public class Player : MonoBehaviour {
         
 
         transform.Translate(horizontalDirection + verticalDirection);
+    }
+
+    private void laserController()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+             shootLaser();
+    }
+
+    private void shootLaser()
+    {
+        if (Time.time > canFire)
+            {
+                Vector3 relativeSpawnPos = new Vector3(0.0f, 0.8f);
+                Instantiate(laserPrefab, transform.position + relativeSpawnPos, Quaternion.identity);
+                canFire = Time.time + fireRate;
+            }
     }
 }
